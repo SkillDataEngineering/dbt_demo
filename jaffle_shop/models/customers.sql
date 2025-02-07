2709339a1644892a9b8e6e6a063b915c6608aef5
Björn Bergdahl <bjorn.bergdahl@molk.com>
2025-02-07 12:11:04 +0100

    tested some

--- jaffle_shop/models/customers.sql
+++ jaffle_shop/models/customers.sql
@@ -1,43 +1,32 @@
 with customers as (
-
     select
         id as customer_id,
         first_name,
         last_name
 
-    from `dbt_demo`.jaffle_shop.customers
-
+    from `dbt_molnet`.customers
 ),
 
 orders as (
-
     select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
-
-    from `dbt_molnet`.jaffle_shop.orders
-
+    from `dbt_molnet`.orders
 ),
 
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
-
     group by 1
-
 ),
 
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -45,11 +34,8 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
     from customers
-
     left join customer_orders using (customer_id)
-
 )
 
 select * from final
